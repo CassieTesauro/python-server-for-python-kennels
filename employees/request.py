e7760a0bd4bb3b3a9fa1a9d26897742838706a30
CassieTesauro <cassie.tesauro@gmail.com>
2021-10-14 15:19:46 -0500

    chapter 7 code

--- employees/request.py
+++ employees/request.py
@@ -78,4 +78,4 @@ def update_employee(id, new_employee):
     for index, employee in enumerate(EMPLOYEES):
         if employee["id"] == id:
            EMPLOYEES[index] = new_employee
-            break
+           break
---
+++ models/customer.py
@@ -0,0 +1,10 @@
+#from ch. 7
+
+class Customer():
+
+    def __init__(self, id, name, email, password):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+      
