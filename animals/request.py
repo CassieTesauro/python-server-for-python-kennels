671379adb9d80e29e9aa1f033a74aeb00cc4840f
CassieTesauro <cassie.tesauro@gmail.com>
2021-10-19 09:09:30 -0500

    code through chapter 9

--- animals/request.py
+++ animals/request.py
@@ -1,3 +1,8 @@
+import sqlite3 #ch 9
+import json  #ch 9
+from models import Animal #ch 9
+
+
 ANIMALS = [
     {
         "id": 1,
@@ -26,23 +31,78 @@ ANIMALS = [
 ]
 
 
-def get_all_animals():
-    return ANIMALS
+def get_all_animals():  #refactored from just being 'return ANIMALS'
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.db") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.location_id,
+            a.customer_id
+        FROM animal a
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        animals = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            animal = Animal(row['id'], row['name'], row['breed'],
+                            row['status'], row['location_id'],
+                            row['customer_id'])
+
+            animals.append(animal.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(animals)
 
 # Function with a single parameter
-def get_single_animal(id):
-    # Variable to hold the found animal, if it exists
-    requested_animal = None
+def get_single_animal(id):  #refactored in ch 9
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    # Iterate the ANIMALS list above. Very similar to the
-    # for..of loops you used in JavaScript.
-    for animal in ANIMALS:
-        # Dictionaries in Python use [] notation to find a key
-        # instead of the dot notation that JavaScript used.
-        if animal["id"] == id:
-            requested_animal = animal
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.location_id,
+            a.customer_id
+        FROM animal a
+        WHERE a.id = ?
+        """, ( id, ))
 
-    return requested_animal
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an animal instance from the current row
+        animal = Animal(data['id'], data['name'], data['breed'],
+                            data['status'], data['location_id'],
+                            data['customer_id'])
+
+        return json.dumps(animal.__dict__)
+
     
 #from ch4
 def create_animal(animal):
@@ -74,8 +134,6 @@ def delete_animal(id):
 
 
 def update_animal(id, new_animal):
-    # Iterate the ANIMALS list, but use enumerate() so that
-    # you can access the index value of each item.
     for index, animal in enumerate(ANIMALS):
         if animal["id"] == id:
             # Found the animal. Update the value.
--- request_handler.py
+++ request_handler.py
@@ -1,45 +1,37 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
-from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
-from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
-from locations import get_all_locations, get_single_location, create_location, delete_location, update_location
 import json
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
+from customers import get_all_customers, get_single_customer, create_customer
+from customers import delete_customer, update_customer
+from employees import get_all_employees, get_single_employee, create_employee
+from employees import delete_employee, update_employee
+from locations import get_all_locations, get_single_location, create_location
+from locations import delete_location, update_location
 
-# Here's a class. It inherits from another class.
-# For now, think of a class as a container for functions that
-# work together for a common purpose. In this case, that
-# common purpose is to respond to HTTP requests from a client.
+
+
 class HandleRequests(BaseHTTPRequestHandler):
-    # This is a Docstring it should be at the beginning of all classes and functions
-    # It gives a description of the class or function
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
-
     def parse_url(self, path):
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
+        """Parses path to access id
+        """
         path_params = path.split("/")
         resource = path_params[1]
         id = None
 
-        # Try to get the item at index 2
         try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
             id = int(path_params[2])
         except IndexError:
-            pass  # No route parameter exists: /animals
+            pass 
         except ValueError:
-            pass  # Request had trailing slash: /animals/
+            pass  
 
         return (resource, id)  # This is a tuple
 
 
     # Here's a class function
     def _set_headers(self, status):
-        # Notice this Docstring also includes information about the arguments passed to the function
         """Sets the status code, Content-Type and Access-Control-Allow-Origin
         headers on the response
 
@@ -51,7 +43,6 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
-    # Another method! This supports requests with the OPTIONS verb.
     def do_OPTIONS(self):
         """Sets the options headers
         """
@@ -63,9 +54,9 @@ class HandleRequests(BaseHTTPRequestHandler):
                         'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any GET request.
     def do_GET(self):
+        """Handles GET requests to the server
+        """
         self._set_headers(200)
         response = {}  # Default response
 
@@ -101,8 +92,6 @@ class HandleRequests(BaseHTTPRequestHandler):
 
 
 
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any POST request.
     def do_POST(self):
         """Handles POST requests to the server
         """
@@ -119,9 +108,6 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Initialize new animal
         new_animal = None
 
-        # Add a new animal to the list. Don't worry about
-        # the orange squiggle, you'll define the create_animal
-        # function next.
         if resource == "animals":
             new_animal = create_animal(post_body)
 
@@ -169,10 +155,6 @@ class HandleRequests(BaseHTTPRequestHandler):
 
             self.wfile.write("".encode())
 
-
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any PUT request.
-
     def do_PUT(self):
         """Handles PUT requests to the server
         """
@@ -198,8 +180,6 @@ class HandleRequests(BaseHTTPRequestHandler):
         if resource == "locations":
             update_location(id, post_body)
         self.wfile.write("".encode())
-
-
 
 # This function is not inside the class. It is the starting
 # point of this application.
